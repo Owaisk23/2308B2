c4c3833ee2fd84e36b364b35bc1a137d2354304e
Owais Ahmed Khan <owais1566@gmail.com>
2024-07-31 10:05:24 +0500

    added Laravel class code

--- Laravel/firstproject/resources/views/welcome.blade.php
+++ Laravel/firstproject/resources/views/welcome.blade.php
@@ -1,1 +1,3 @@
 <h1>First project in Laravel</h1>
+<a href="/about">About Page</a><br><br>
+<a href="/contact">Contact Page</a>
--- Laravel/firstproject/routes/web.php
+++ Laravel/firstproject/routes/web.php
@@ -10,3 +10,15 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('about');
 });
+// Another Way
+Route::view('/contact', 'contact');
+
+// Routes with required parameter
+// Route::get('/service/{para}', function () {
+//     return view('service');
+// });
+
+// Routes with optional parameter
+// Route::get('/service/{serviceparam?}', function () {
+//     return view('service');
+// });
