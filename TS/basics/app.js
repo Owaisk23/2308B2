8deac57c9d380837d9b7250ec29e865c3d2d9477
Owais Ahmed Khan <owais1566@gmail.com>
2024-09-06 10:27:58 +0500

    added interfaces and arrays n objects with methods

--- TS/basics/app.js
+++ TS/basics/app.js
@@ -1,26 +1,85 @@
-console.log('Intro to typescript');
-var userName = "Owais Ahmed Khan";
+console.log("Intro to typescript");
+// let userName: string = "Owais Ahmed Khan";
 // console.log(userName);
 // console.log(typeof userName);
 // console.log(userName.toLowerCase())
 // console.log(userName.toUpperCase())
-var userAge = 25;
+// let userAge: number = 25;
 // console.log(userAge)
 //template literals: backtics``
-var userDetails = "User name is ".concat(userName.toUpperCase(), " and User age is ").concat(userAge, " and User lives in city Karachi");
+// let userDetails = `User name is ${userName.toUpperCase()} and User age is ${userAge} and User lives in city Karachi`;
 // console.log(userDetails)
 // Null = 0  True: 1 False: 0
-var nullChecker = null;
-if (nullChecker) {
-    console.log('null true huta hai');
-}
-else {
-    console.log('null false huta hai');
-}
-var undefinedChecker = undefined;
-if (undefinedChecker) {
-    console.log('undefined true huta hai');
-}
-else {
-    console.log('undefined false huta hai');
-}
+// let nullChecker = null;
+// if(nullChecker){
+//     console.log('null true huta hai');
+// }else{
+//     console.log('null false huta hai');
+// }
+// let undefinedChecker = undefined;
+// if(undefinedChecker){
+//     console.log('undefined true huta hai');
+// }else{
+//     console.log('undefined false huta hai');
+// }
+// Arrays
+// let stdMarks: number[] = [55, 88, 95, 100]
+// console.log(stdMarks)
+// let stdNames: string[] = ["Abdullah", "Neha", "Nimra", "Rubbas", "Sameer", "Saif", "Tayyaba"];
+// // console.log(stdNames)
+// stdNames.forEach((val)=>{
+//     console.log(val)
+// })
+// let stds = stdNames.map((val)=>{
+//     console.log(val)
+//     return val.toUpperCase()
+// })
+// console.log(stds)
+// Object
+// let product: any = {
+//   productName: "IPhone 15 Pro Max",
+//   productPrice: 3500,
+//   productColor: "Purple",
+//   productInStock: true,
+// };
+// console.log(product);
+// console.log(product.productName);
+// console.log(product.productColor);
+// let user: object = {
+//     userName: "Abdullah",
+//     userAge: 23,
+//     userActive: true
+// }
+// console.log(user)
+// let user: {
+//   userName: string;
+//   userAge: number;
+//   userActive: boolean;
+// } = {
+//   userName: "Abdullah",
+//   userAge: 23,
+//   userActive: true,
+// };
+// console.log(user)
+// let mixedDataTypes: (string | number)[];
+// mixedDataTypes = ["bmw", "Audi", "Mehran", 23000, 45000, 3200];
+// mixedDataTypes.forEach((val) => {
+//     // console.log(val)
+//     if(typeof val == "string"){
+//         console.log(val.toUpperCase())
+//     }else{
+//         console.log(val.toFixed(2))
+//     }
+// })
+var teacher = {
+    name: "Owais",
+    exp: 5
+};
+var obj1 = {
+    stdId: 7666,
+    stdName: "Saad",
+    teacher_Id: 555,
+    teacher_Name: "Afzal"
+};
+console.log(obj1["teacher_Name"]);
+console.log(obj1.stdName);
--- TS/basics/app.ts
+++ TS/basics/app.ts
@@ -1,38 +1,158 @@
-console.log('Intro to typescript')
+console.log("Intro to typescript");
 
-let userName: string = "Owais Ahmed Khan";
+// let userName: string = "Owais Ahmed Khan";
 
 // console.log(userName);
 // console.log(typeof userName);
 // console.log(userName.toLowerCase())
 // console.log(userName.toUpperCase())
 
-let userAge: number = 25;
+// let userAge: number = 25;
 
 // console.log(userAge)
 
 //template literals: backtics``
-let userDetails = `User name is ${userName.toUpperCase()} and User age is ${userAge} and User lives in city Karachi`;
+// let userDetails = `User name is ${userName.toUpperCase()} and User age is ${userAge} and User lives in city Karachi`;
 
 // console.log(userDetails)
 
 // Null = 0  True: 1 False: 0
-let nullChecker = null;
+// let nullChecker = null;
 
-if(nullChecker){
-    console.log('null true huta hai');
-}else{
-    console.log('null false huta hai');
+// if(nullChecker){
+//     console.log('null true huta hai');
+// }else{
+//     console.log('null false huta hai');
+// }
+
+// let undefinedChecker = undefined;
+
+// if(undefinedChecker){
+//     console.log('undefined true huta hai');
+// }else{
+//     console.log('undefined false huta hai');
+// }
+
+// Arrays
+
+// let stdMarks: number[] = [55, 88, 95, 100]
+// console.log(stdMarks)
+
+// let stdNames: string[] = ["Abdullah", "Neha", "Nimra", "Rubbas", "Sameer", "Saif", "Tayyaba"];
+// // console.log(stdNames)
+
+// stdNames.forEach((val)=>{
+//     console.log(val)
+// })
+
+// let stds = stdNames.map((val)=>{
+//     console.log(val)
+//     return val.toUpperCase()
+// })
+
+// console.log(stds)
+
+// Object
+
+// let product: any = {
+//   productName: "IPhone 15 Pro Max",
+//   productPrice: 3500,
+//   productColor: "Purple",
+//   productInStock: true,
+// };
+
+// console.log(product);
+// console.log(product.productName);
+// console.log(product.productColor);
+
+// let user: object = {
+//     userName: "Abdullah",
+//     userAge: 23,
+//     userActive: true
+// }
+
+// console.log(user)
+
+// let user: {
+//   userName: string;
+//   userAge: number;
+//   userActive: boolean;
+// } = {
+//   userName: "Abdullah",
+//   userAge: 23,
+//   userActive: true,
+// };
+
+// console.log(user)
+
+// let mixedDataTypes: (string | number)[];
+
+// mixedDataTypes = ["bmw", "Audi", "Mehran", 23000, 45000, 3200];
+
+// mixedDataTypes.forEach((val) => {
+//     // console.log(val)
+//     if(typeof val == "string"){
+//         console.log(val.toUpperCase())
+//     }else{
+//         console.log(val.toFixed(2))
+//     }
+// })
+
+
+let teacher: {name: string, exp: number } = {
+    name: "Owais",
+    exp: 5
+}
+ 
+// // Aliased Object Type 
+// type Student = {
+//     name: string,
+//     age?: number
+// }
+
+// let stds: Student = {
+//     name:"Wajeeha",
+//     age: 23
+// }
+// console.log(stds["name"])
+// console.log(stds.age)
+
+// Interfaces
+
+// interface Manager {
+//     name: string,
+//     salary?: number
+// }
+
+// let storeManager: Manager = {
+//     name: "Hassan",
+//     salary: 450000
+// }
+
+// Intersection types
+
+interface Teacher {
+    teacher_Id: number,
+    teacher_Name: string
 }
 
-
-let undefinedChecker = undefined;
-
-if(undefinedChecker){
-    console.log('undefined true huta hai');
-}else{
-    console.log('undefined false huta hai');
+interface Student {
+    stdId: number,
+    stdName: string
 }
+
+type intersected_types = Teacher & Student;
+
+let obj1: intersected_types = {
+    stdId: 7666,
+    stdName: "Saad",
+    teacher_Id: 555,
+    teacher_Name: "Afzal"
+}
+
+console.log(obj1["teacher_Name"])
+console.log(obj1.stdName)
+
 
 
 
