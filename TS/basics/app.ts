50aec4927470b3a7555ee3bbf948da112f97d3a6
Owais Ahmed Khan <owais1566@gmail.com>
2024-09-06 10:45:34 +0500

    added tuples work

--- TS/basics/app.ts
+++ TS/basics/app.ts
@@ -131,28 +131,71 @@ let teacher: {name: string, exp: number } = {
 
 // Intersection types
 
-interface Teacher {
-    teacher_Id: number,
-    teacher_Name: string
+// interface Teacher {
+//     teacher_Id: number,
+//     teacher_Name: string
+// }
+
+// interface Student {
+//     stdId: number,
+//     stdName: string
+// }
+
+// type intersected_types = Teacher & Student;
+
+// let obj1: intersected_types = {
+//     stdId: 7666,
+//     stdName: "Saad",
+//     teacher_Id: 555,
+//     teacher_Name: "Afzal"
+// }
+
+// console.log(obj1["teacher_Name"])
+// console.log(obj1.stdName)
+
+
+// Tuple types have advantages that you can accurately describe the type of an array
+
+var tuple: [number, string] = [1, "Bob"];
+var scndElement = tuple[1]
+// console.log(scndElement)
+console.log(tuple[1])
+
+// In JS
+const failingResponse = ["Non Found", 404]
+//  In TS
+const passingResponse: [string, number] = ["{}", 200]
+
+if(passingResponse[1] === 200){
+    const localInfo = JSON.parse(passingResponse[0])
+    console.log(localInfo)
 }
 
-interface Student {
-    stdId: number,
-    stdName: string
-}
 
-type intersected_types = Teacher & Student;
+type staffAccount = [number, string, string, string?]
 
-let obj1: intersected_types = {
-    stdId: 7666,
-    stdName: "Saad",
-    teacher_Id: 555,
-    teacher_Name: "Afzal"
-}
+const staff: staffAccount[] = [
+    [0, "Hassan", "hassan@gmail.com", "CAH"],
+    [1, "Ali", "ali@gmail.com"],
+    [2, "Saad", "saad@gmail.com"],
+    [3, "Wajeeha", "wajeeha@gmail.com"]
+];
 
-console.log(obj1["teacher_Name"])
-console.log(obj1.stdName)
+type PaySTubs = [staffAccount, ...number[]];
 
+const payStubs: PaySTubs[] = [
+    [staff[0], 300],
+    [staff[1], 250, 300],
+    [staff[2], 300, 400, 250],
+];
+
+const monthOnePayments = payStubs[0][1] + payStubs[1][1] + payStubs[2][1]
+const monthTwoPayments = payStubs[1][2] + payStubs[2][2];
+const monthThreePayments = payStubs[2][2];
+
+console.log(monthOnePayments)
+console.log(monthTwoPayments)
+console.log(monthThreePayments)
 
 
 
