4adb1fb3bec712e0d644f70b1337767986bb6f99
Owais Ahmed Khan <owais1566@gmail.com>
2024-09-23 10:55:21 +0500

    Angular class code

---
+++ Angular/databinding/src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-profile',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-profile.component.html',
+  styleUrl: './user-profile.component.css'
+})
+export class UserProfileComponent {
+  name= "Saif";
+  designation= "Software Developer";
+  salary= 72000;
+  inputVal = "test"
+  users = [
+    {name: "Affan", eligibleStatus: true, age: 23,},
+    {name: "Saif", eligibleStatus: false, age: 27,},
+    {name: "Rubbas", eligibleStatus: false, age: 25,},
+    {name: "Hasnain", eligibleStatus: true, age: 24,}
+  ]
+
+
+  isBtnDisable = true;
+
+  onChangeFunc(e: Event){
+    const val = (e.target as HTMLInputElement).value;
+    this.inputVal = val
+  }
+}
