0bd7d2a66a9219751575de3f2977d7149eccbeb0
Owais Ahmed Khan <owais1566@gmail.com>
2024-09-27 10:47:57 +0500

    added data binding

--- Angular/databinding/src/app/app.component.ts
+++ Angular/databinding/src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserProfileComponent } from './user-profile/user-profile.component'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserProfileComponent],
+  imports: [RouterOutlet, UserProfileComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'databinding';
+  users = [
+    {name: "Affan", eligibleStatus: true, age: 23,},
+    {name: "Saif", eligibleStatus: false, age: 27,},
+    {name: "Rubbas", eligibleStatus: false, age: 25,},
+    {name: "Hasnain", eligibleStatus: true, age: 24,}
+  ]
 }
--- Angular/databinding/src/app/user-profile/user-profile.component.ts
+++ Angular/databinding/src/app/user-profile/user-profile.component.ts
@@ -1,6 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { booleanAttribute, Component, Input, numberAttribute } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
+function formatName(value: string){
+  return "Welcome " + value;
+}
+
 
 @Component({
   selector: 'app-user-profile',
@@ -10,17 +15,19 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-profile.component.css'
 })
 export class UserProfileComponent {
-  @Input({alias: "userName"}) name = "";
+  @Input({alias: "userName", transform: formatName}) name = "";
+  @Input({transform: booleanAttribute}) eligibleStatus!: boolean;
+  @Input({transform: numberAttribute}) age!: number;
   // name= "Saif";
   // designation= "Software Developer";
   // salary= 72000;
   // inputVal = "test"
-  // users = [
-  //   {name: "Affan", eligibleStatus: true, age: 23,},
-  //   {name: "Saif", eligibleStatus: false, age: 27,},
-  //   {name: "Rubbas", eligibleStatus: false, age: 25,},
-  //   {name: "Hasnain", eligibleStatus: true, age: 24,}
-  // ]
+  users = [
+    {name: "Affan", eligibleStatus: true, age: 23,},
+    {name: "Saif", eligibleStatus: false, age: 27,},
+    {name: "Rubbas", eligibleStatus: false, age: 25,},
+    {name: "Hasnain", eligibleStatus: true, age: 24,}
+  ]
 
 
   // isBtnDisable = true;
