821b8c88c023bdcfd404f3e2a63e77967da9d328
Owais Ahmed Khan <owais1566@gmail.com>
2025-01-08 12:55:12 +0500

    added validation

---
+++ Dotnet/validation/validation/Models/Model.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace validation.Models
+{
+
+    public class Model
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [RegularExpression("^([\\w\\.\\-]+)@([\\w\\-]+)((\\.(\\w){2,3})+)$")]
+        public string email { get; set; }
+
+        [Required]
+        [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^\\da-zA-Z]).{8,15}$")]
+        public string pwd { get; set; }
+
+
+
+
+    }
+}
