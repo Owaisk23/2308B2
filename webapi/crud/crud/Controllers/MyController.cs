e25da8f9122f85e50f3d47ec31b164354d4808c0
owaisk23 <owais1566@gmail.com>
2025-03-19 10:08:59 +0500

    webapi crud

---
+++ webapi/crud/crud/Controllers/MyController.cs
@@ -0,0 +1,39 @@
+﻿using crud.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace crud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyController : ControllerBase
+    {
+        private readonly CrudContext db;
+
+        public MyController(CrudContext _db)
+        {
+            db = _db;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(db.Cruds.ToList());
+        }   
+        [HttpPost]
+        public IActionResult AddProducts(Crud crud)
+        {
+            db.Cruds.Add(crud);
+            db.SaveChanges();
+            return StatusCode(201);
+        } 
+        [HttpDelete]
+        public IActionResult DeleteProduct(int id)
+        {
+            var product = db.Cruds.Find(id);
+            db.Cruds.Remove(product);
+            db.SaveChanges();
+            return StatusCode(201);
+        }
+    }
+}
---
+++ webapi/crud/crud/Models/Crud.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace crud.Models;
+
+public partial class Crud
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public int Price { get; set; }
+}
