490140d1c4e7c849adad9b7205c7669a1bf0c24a
owaisk23 <owais1566@gmail.com>
2025-04-28 10:52:39 +0500

    added first screen

---
+++ MobAppDevelopment/Flutter/firstproject/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:firstproject/firstScreen.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: FirstScreen()
+    )
+  );
+}
