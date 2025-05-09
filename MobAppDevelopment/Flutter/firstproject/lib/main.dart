a28e814b1321ca85b11bd95be10763f211a711d4
owaisk23 <owais1566@gmail.com>
2025-05-09 10:44:42 +0500

    added product screen

--- MobAppDevelopment/Flutter/firstproject/lib/main.dart
+++ MobAppDevelopment/Flutter/firstproject/lib/main.dart
@@ -1,13 +1,25 @@
-// import 'package:firstproject/firstScreen.dart';
-import 'package:firstproject/scndScreen.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:firstproject/product.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(
-    MaterialApp(
-      title: 'Flutter Demo',
+void main() => runApp(
+  DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => MyApp(), // Wrap your app
+  ),
+);
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
-      home: ScndScreen()
-    )
-  );
+      home: Product(),
+    );
+  }
 }
---
+++ MobAppDevelopment/Flutter/firstproject/lib/product.dart
@@ -0,0 +1,115 @@
+import 'package:firstproject/scndScreen.dart';
+import 'package:flutter/material.dart';
+
+class Product extends StatefulWidget {
+  const Product({ Key? key }) : super(key: key);
+
+  @override
+  _ProductState createState() => _ProductState();
+}
+
+class _ProductState extends State<Product> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Product Card'),
+        backgroundColor: Colors.black87,
+        foregroundColor: Colors.white,
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Container(
+          height: 500,
+          width: 300,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(
+              color: Colors.black,
+              width: 2,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Column(
+              children: [
+                Image.network(
+                  'https://hips.hearstapps.com/hmg-prod/images/m3-macbook-air-02-large-65f1a73f08f9b.jpeg?crop=0.667xw:1.00xh;0.147xw,0&resize=980:*',
+                  height: 250,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'MacBook Air',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '\$999',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'The MacBook Air is a line of laptop computers developed by Apple Inc.Everyday for professional and personal use.',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey[700],
+                  ),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        // Add to Cart action
+                      },
+                      child: Text('Add to Cart'),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        foregroundColor: Colors.white,
+                        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => ScndScreen()));
+                      },
+                      child: Text('Buy Now'),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        foregroundColor: Colors.white,
+                        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            
+            
+            ),
+        ) 
+        
+      ),
+    );
+  }
+}
