a79ed212a70b5dce573bf3c06f11d9fb2a5d0bc6
owaisk23 <owais1566@gmail.com>
2025-06-16 10:45:49 +0500

    added

--- MobAppDevelopment/Flutter/firebase_crud/lib/main.dart
+++ MobAppDevelopment/Flutter/firebase_crud/lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crud/addproduct.dart';
+import 'package:firebase_crud/firebase_options.dart';
 import 'package:firebase_crud/splashScreen.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+
+void main()async {
+
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: SplashScreen(),
