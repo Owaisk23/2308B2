8d59c8256175f6d95a364b24da222f179a8d986a
owaisk23 <owais1566@gmail.com>
2025-06-25 10:28:43 +0500

    added signup

--- MobAppDevelopment/Flutter/crud/lib/main.dart
+++ MobAppDevelopment/Flutter/crud/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:crud/addproduct.dart';
 import 'package:crud/firebase_options.dart';
 import 'package:crud/products.dart';
+import 'package:crud/signup.dart';
+import 'package:crud/splashScreen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +13,13 @@ void main() async {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Products()
+      home: SplashScreen(),
+      routes: {
+        "/signup":(context)=> Signup(),
+        "/products":(context)=>  Products(),
+        // "/add":(context)=> (isLoggedIn && isAdmin) ? Addproduct(): Login(),
+        // "/login":(context)=>Login(),
+      },
       )
     );
 }
