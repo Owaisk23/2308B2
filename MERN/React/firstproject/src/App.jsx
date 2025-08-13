6856c68d06df601255e83827db33ccb649c9329e
owaisk23 <owais1566@gmail.com>
2025-08-13 10:45:10 +0500

    added react basics

---
+++ MERN/React/firstproject/src/App.jsx
@@ -0,0 +1,17 @@
+import MyName from "./components/MyName"
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="heading">Hello In React</h1>
+      <MyName/>
+      <MyName/>
+      <MyName/>
+      <MyName/>
+    </>
+  )
+}
+
+export default App
---
+++ MERN/React/firstproject/src/components/MyName.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MyName = () => {
+  return (
+    <>
+      <h1>My name is Owais Ahmed Khan.</h1>
+    </>
+  );
+};
+
+export default MyName;
