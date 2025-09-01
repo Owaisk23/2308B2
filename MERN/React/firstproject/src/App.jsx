44a9f97246530f298dd35f1e481748a39d337bbb
owaisk23 <owais1566@gmail.com>
2025-09-01 10:46:38 +0500

    added toggle bulb

--- MERN/React/firstproject/src/App.jsx
+++ MERN/React/firstproject/src/App.jsx
@@ -3,8 +3,19 @@ import Navbar from "./components/Navbar"
 import KidsImg from "./assets/kids.jpg";
 import Card from "./components/Card";
 import Footer from "./components/Footer";
+import { useState } from "react";
+import Bulb from "./components/Bulb";
 
 function App() {
+
+  // let count = 0;
+  //useState
+  const [count, setCount] = useState(0);
+  const [user, setUser] = useState({
+    name: "Haris Naseer",
+    age: 25,
+    prfession: "Software Developer"
+  })
 
   let userData = [
     {
@@ -59,10 +70,38 @@ function App() {
     },
   ];
 
+  // function handleIncrementCount(){
+  //   count++;
+  //   console.log(count);
+  // }
+
+  const handleIncrementCount = () => {
+    setCount(count + 1);
+    console.log(count)
+  }
+
+  const updateUser = () => {
+    setUser({...user, name: "Owais Ahmed Khan"});
+  }
+
+
   return (
     <>
       <Navbar/>
       <h1 className="heading bg-success">Hello In React</h1>
+
+      {/* Use State start */}
+      <h2>{count}</h2>
+      {/* <button className="btn btn-primary" onClick={() => {setCount(count + 1)}}>Increment</button> */}
+      <button className="btn btn-primary" onClick={handleIncrementCount}>Increment</button>
+
+      <h2>User Name: {user.name} User Age: {user.age} User Profession: {user.prfession}</h2>
+      <button className="btn btn-warning" onClick={updateUser}>Update User</button>
+
+      <Bulb/>
+
+      {/* Use State end */}
+
 
       {/* <h2>{userData[1].name}</h2>
       <h2>{userData[2].gender}</h2>
@@ -73,20 +112,20 @@ function App() {
       <MyName name={"Mahnoor"} gender={"Female"}/>
       <MyName name={"Fahad"} gender={"Male"}/> */}
 
-      {
+      {/* {
         userData.map((item, index) => {
           return <MyName key={index} name={item.name} gender={item.gender}/>
         })
-      }
+      } */}
 
-       <div className="d-flex justify-content-around mt-3">
+       {/* <div className="d-flex justify-content-around mt-3">
        {
           cardData.map((item, index) => {
           return <Card key={index} image={item.image} title={item.title} description={item.description} />
             })
         }
        </div>
-      <img src={KidsImg} alt="" />
+      <img src={KidsImg} alt="" /> */}
 
       <Footer/>
     </>
---
+++ MERN/React/firstproject/src/components/Bulb/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import onBulb from "../../assets/bulbOn.png";
+import offBulb from "../../assets/bulbOff.png";
+import { useState } from "react";
+
+const Bulb = () => {
+  const [isOn, setIsOn] = useState(true);
+  return (
+    <div>
+      <h1>Bulb is {isOn ? "On" : "Off"}</h1>
+      <button className="btn btn-primary" onClick={() => setIsOn(!isOn)}>
+        Toggle
+      </button>
+      {isOn ? (
+        <div className="img-container">
+          <img src={onBulb} alt="" />
+        </div>
+      ) : (
+        <div className="img-container">
+          <img src={offBulb} alt="" />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Bulb;
