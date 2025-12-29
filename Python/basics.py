55ef8eb7a93ac7e8e31154c6e6580605fe9a8e41
owaisk23 <owais1566@gmail.com>
2025-12-29 09:13:53 +0500

    basics of python

---
+++ Python/basics.py
@@ -0,0 +1,31 @@
+##import math
+##print("Assalam O Alaikum Python")
+a = 23
+b = 12
+##print(a + b)
+##print(math.floor(7.65))
+##print(math.cos(3.65))
+
+##print("Addition", a + b)
+##print("Substraction", a - b)
+##print("Multiply", a * b)
+##print("Division", a / b)
+##print("Floor Division", a // b)
+##print("Modulus", a % b)
+##print("Exponential", a ** b)
+##
+##name = input("Enter your name: ")
+##print("UserName: ", name)
+##
+##num = int(input("Enter number: "))
+##print("Expo Num: ", num ** num)
+
+
+fruits = ["Apple", "Cherry", "Grapes"]
+print(fruits)
+fruits.append('orange')
+print(fruits)
+
+fruits.insert(2, 'Kiwi')
+print(fruits)
+
